27a3d613a0492ef86a574276adedc0e5b3ca518e
jasonharmongit <contactjasonharmon@gmail.com>
2025-04-09 12:49:04 -0600

    cleaning up

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 export default function Home() {
   return (
-    <div className="min-h-screen">
-      <header>
-        <div className="container py-4">
+    <div className="min-h-screen w-screen max-w-none m-0 p-0">
+      <header className="w-full border-b border-gray-200">
+        <div className="container mx-left py-4 pl-4">
           <h1 className="text-2xl font-medium tracking-tight">
             <span>JASON</span>
             <span className="text-teal-400">data</span>
+            <span className="ml-4">|<span className="ml-4">Projects</span></span>
           </h1>
         </div>
       </header>
